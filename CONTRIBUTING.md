06256c74dcd5b41d20a6cde1a66fcb3a1af665b5
Naka Masato <masatonaka1989@gmail.com>
2023-02-07 10:31:06 +0900

    feat: remove html from autonote package (#24)

--- CONTRIBUTING.md
+++ CONTRIBUTING.md
@@ -57,6 +57,16 @@ Ready to contribute? Here's how to set up `autonote` for local development.
 
 4. When you're done making changes, check that your changes conform to any code formatting requirements and pass any tests.
 
+    1. Test example scripts.
+        ```
+        poetry self add poetry-dotenv-plugin
+        ```
+
+    1. Prepare `.env` file. (Will actually create a Confluence page)
+        ```
+        poetry run python examples/create_confluence_page.py
+        ```
+
 5. Commit your changes and open a pull request.
 
 ## Pull Request Guidelines
--- examples/create_confluence_page.py
+++ examples/create_confluence_page.py
@@ -1,10 +1,32 @@
-from autonote.confluence import ConfluenceClient
-from autonote.html import generate
+from datetime import date, timedelta
+from os import environ, path
 
-content = generate()
+from jinja2 import Environment, FileSystemLoader
+
+from autonote.confluence import ConfluenceClient
+
+data = {
+    "weeks": [
+        {
+            "start_date": "2023/02/06",
+            "end_date": "2023/02/10",
+        },
+        {
+            "start_date": "2023/02/20",
+            "end_date": "2023/02/24",
+        },
+        {
+            "start_date": "2023/02/07",
+            "end_date": "2023/03/03",
+        },
+    ]
+}
+env = Environment(loader=FileSystemLoader(path.dirname(__file__)))
+template = env.get_template("templates/monthly_report.html.tpl")
+content = template.render(data)
 client = ConfluenceClient()
 client.create_page(
-    "<confluence_parent_page_id>",
-    title="title",
+    parent_page_id=environ["CONFLUENCE_PARENT_PAGE_ID"],
+    title=f"2023/02/07 Feb test",
     body=content,
 )
